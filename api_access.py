2426117fcd169ede23c4600cd80c02aad12c1710
Connor V <11cvantress@gmail.com>
2024-03-09 18:48:01 -0600

    Check if stock ticker is valid function completed

--- api_access.py
+++ api_access.py
@@ -1,5 +1,4 @@
 #Project imports
-
 import requests
 
 
@@ -7,27 +6,32 @@ import requests
 
 # Check if given ticker symbol points to a real stock 
 def check_stock(ticker):
-    return
+
+    response = requests.get(url='https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + ticker + '&interval=5min&apikey=JLFXYX4J4I20CF8E')
+    data = response.json()
+
+    if 'Error Message' in data:
+        print("It does not seem that this ticker symbol points to an existing stock. Please try again")
+    else: 
+        print('The ticker symbol ' + ticker + ' is valid')
+        print(data)
+        return ticker
+
 
 def get_data_from_api(time_series, ticker):
 
     #time_series must be one of these four:
-    # - TIME_SERIES_INTRA
+    # - TIME_SERIES_INTRADAY
     # - TIME_SERIES_DAILY
     # - TIME_SERIES_WEEKLY
     # - TIME_SERIES_MONTHLY  
 
-    url = 'https://www.alphavantage.co/query?function=' + time_series + '&symbol=' + ticker + '&interval=60min0&apikey=JLFXYX4J4I20CF8E'
+    url = 'https://www.alphavantage.co/query?function=' + time_series + '&symbol=' + ticker + '&interval=5min&apikey=JLFXYX4J4I20CF8E'
+
     r = requests.get(url)
     data = r.json()
 
     print(data)
-    
-    # #Proof of concept 
 
-    # url = ""
 
-    # # Make API request 
-    # response = requests.get()
-
-get_data_from_api()
+# get_data_from_api('TIME_SERIES_INTRADAY', 'IB')
