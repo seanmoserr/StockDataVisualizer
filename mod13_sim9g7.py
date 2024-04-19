86749d24c61db425078d1f00a91d9e4227fd330f
seanmoserr <115335942+seanmoserr@users.noreply.github.com>
2024-04-19 12:50:11 -0500

    Unit tests for stock data visualizer

--- mod13_sim9g7.py
+++ mod13_sim9g7.py
@@ -4,23 +4,23 @@ import re
 
 class sdvUnitTests(unittest.TestCase):
 
-    def test_symbol_constraint(self):
+    def tSymbol(self):
         symbol = "AMD"
         self.assertTrue(re.match("^[A-Z]{1,7}$", symbol))
 
-    def test_chart_type_constraint(self):
+    def tChartType(self):
         chart_type = "2"
         self.assertTrue(chart_type in ["1", "2"])
 
-    def test_time_series_constraint(self):
+    def tTimeSeries(self):
         time_series = "3"
         self.assertTrue(re.match("^[1-4]$", time_series))
 
-    def test_start_date_constraint(self):
+    def tSDate(self):
         start_date = "2021-05-05"
         self.assertTrue(re.match("^(\d{4})-(\d{2})-(\d{2})$", start_date))
 
-    def test_end_date_constraint(self):
+    def tEDate(self):
         end_date = "2022-06-06"
         self.assertTrue(re.match("^(\d{4})-(\d{2})-(\d{2})$", end_date))
 
